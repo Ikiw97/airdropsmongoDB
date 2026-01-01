908f15786caa498002662e97b2de2e44a7754a03
kiw <106908925+Ikiw97@users.noreply.github.com>
2026-01-01 13:29:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
   return (
     <SecurityProvider>
       <ThemeProvider>
-        <div className="fixed top-4 right-4 z-50">
+        <div className="fixed bottom-4 right-4 z-50">
           <ThemeToggle />
         </div>
         <AnimatePresence mode="wait">
